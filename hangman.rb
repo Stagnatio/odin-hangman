2e5a9ed2d80e1bc0386b033bcf37cf7db94af7ad
Pickles <cheesemaster56@gmail.com>
2022-05-02 19:23:25 -0400

    Working game

--- hangman.rb
+++ hangman.rb
@@ -1,12 +1,40 @@
 #require 'google-10000-english-no-swears.txt'
+class GAME
+    words = File.readlines('google-10000-english-no-swears.txt')
+    goodWords = Array.new
 
-words = File.readlines('google-10000-english-no-swears.txt')
-#define blank array
-
-def select_word(words)
     words.each do |word|
         if word.length > 5 && word.length < 12
-            #array.add word
+            goodWords.push word
         end
     end
+
+    secret = goodWords[rand(goodWords.length)]
+    puts secret
+    guesses = ""
+    result = ""
+    for i in 0..secret.length-1     #makes a string equal in length to secret
+        result = result + "_"
+    end
+    while (secret != result)
+        puts result
+        puts "Guess a letter: "
+        guesses = guesses + gets
+
+        for i in 0..secret.length-1
+            for j in 0..guesses.length-1
+                if secret[i] == guesses[j]
+                    result[i] = secret[i]
+                    break
+                end
+            end
+        end
+    end
+
+    puts result
+    if result = secret
+        puts "You win!"
+    end
 end
+
+GAME.new
